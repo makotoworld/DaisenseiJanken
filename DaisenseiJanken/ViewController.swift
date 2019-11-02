f9ed959d963fb5e523d958cf74b0cf9587e1b62e
Makoto Nozaki <makotoworld@gmail.com>
2019-11-02 21:43:17 +0900

    Initial Commit

---
+++ DaisenseiJanken/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DaisenseiJanken
+//
+//  Created by Makoto Nozaki on 2019/11/02.
+//  Copyright © 2019 Makoto Nozaki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
